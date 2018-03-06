ab73bb90aa75e2b2e30b4a51d6de5f92608406cc
Mavey Ma <maveyma@gmail.com>
2018-03-05 16:10:52 -0800

    Fixed UI toggle logic for favorite/retweet

--- twitter_alamofire_demo/TweetCell.swift
+++ twitter_alamofire_demo/TweetCell.swift
@@ -28,27 +28,152 @@ class TweetCell: UITableViewCell {
   
   var tweet: Tweet! {
     didSet {
-      tweetTextLabel.text = tweet.text
-      replyCountLabel.text = "" //TODO Deprecated. https://www.quora.com/Is-it-possible-to-get-a-count-of-the-replies-and-retweets-for-a-particular-tweet
-      favoriteCountLabel.text = String(describing: tweet.favoriteCount)
-//      if tweet.favorited! {
-//        let redTapImage = UIImage(named: "favor-icon-red")
-//        favoriteButton.setImage(redTapImage, for: UIControlState.normal)
-//      }
-      retweetCountLabel.text = String(describing: tweet.retweetCount)
-//      if tweet.retweeted {
-//        let greenTapImage = UIImage(named: "retweet-icon-green")
-//        retweetButton.setImage(greenTapImage, for: UIControlState.normal)
-//      }
+      profilePicImageView.layer.cornerRadius = profilePicImageView.frame.height/2
+      profilePicImageView.af_setImage(withURL: URL(string: tweet.user.profilePicturePathString!)!)
       authorNameLabel.text = tweet.user.name
       twitterHandleLabel.text = "@" + tweet.user.screenName!
       timeStampLabel.text = " · " + tweet.createdAtString
       
-      profilePicImageView.layer.cornerRadius = profilePicImageView.frame.height/2
+      replyCountLabel.text = "" //TODO Deprecated. https://www.quora.com/Is-it-possible-to-get-a-count-of-the-replies-and-retweets-for-a-particular-tweet
+      retweetCountLabel.text = String(describing: tweet.retweetCount)
+      favoriteCountLabel.text = String(describing: tweet.favoriteCount)
       
-      profilePicImageView.af_setImage(withURL: URL(string: tweet.user.profilePicturePathString!)!)
+      initButtons()
+      
+      tweetTextLabel.text = tweet.text
     }
   }
+  
+  func initButtons() {
+    // Return true if your favorited value is true, and false otherwise.
+    // The ?? is there just in case your favorited field is nil. If it is nil, then we will just give it a false value just to be safe.
+    retweetButton.isSelected = tweet.retweeted
+    favoriteButton.isSelected = tweet.favorited ?? false
+    
+    if tweet.retweeted {
+      setSelectedRetweetGreenIcon()
+    }
+    if tweet.favorited! {
+      setSelectedFavoriteRedIcon()
+    }
+  }
+
+  @IBAction func didTapFavorite(_ sender: Any) {
+    // Update the favorite icon and count in local tweet model
+    tweet.favorited = !tweet.favorited!
+    favoriteButton.isSelected = tweet.favorited ?? false
+    
+    if tweet.favorited! {
+      setSelectedFavoriteRedIcon()
+      tweet.favoriteCount += 1
+      // Send a POST request to the POST favorites/create endpoint
+      postRequestToFavoritesCreateEndpoint()
+      
+    } else {
+      setNormalUnfavoriteGreyIcon()
+      tweet.favoriteCount -= 1
+      // Send a POST request to the POST favorites/destroy endpoint
+      postRequestToFavoritesDestroyEndpoint()
+    }
+    // Update cell UI
+    refreshTweetCellUI()
+  }
+  
+  @IBAction func didTapRetweet(_ sender: Any) {
+    // Update the retweet icon and count in local tweet model
+    tweet.retweeted = !tweet.retweeted
+    retweetButton.isSelected = tweet.retweeted
+    
+    if tweet.retweeted {
+      setSelectedRetweetGreenIcon()
+      tweet.retweetCount += 1
+      // Send a POST request to the POST statuses/retweet/:id.json endpoint
+      postRequestToStatusesRetweetIDEndpoint()
+    } else {
+      setNormalUnretweetGreyIcon()
+      tweet.retweetCount -= 1
+      // Send a POST request to the POST statuses/unretweet/:id.json endpoint
+      postRequestToStatusesUnretweetIDEndpoint()
+    }
+    // Update cell UI
+    refreshTweetCellUI()
+  }
+  
+  func refreshTweetCellUI() {
+    favoriteCountLabel.text = String(describing: tweet.favoriteCount)
+    retweetCountLabel.text = String(describing: tweet.retweetCount)
+  }
+  
+  //--------------------------------------------------------------------------------//
+  // Helper functions for setting icons
+  func setSelectedFavoriteRedIcon() {
+    let redTapImage = UIImage(named: "favor-icon-red")
+    favoriteButton.setImage(redTapImage, for: UIControlState.selected)
+  }
+  
+  func setSelectedRetweetGreenIcon() {
+    let greenTapImage = UIImage(named: "retweet-icon-green")
+    retweetButton.setImage(greenTapImage, for: UIControlState.selected)
+  }
+  
+  func setNormalUnfavoriteGreyIcon() {
+    let greyTapImage = UIImage(named: "favor-icon")
+    favoriteButton.setImage(greyTapImage, for: UIControlState.normal)
+  }
+  
+  func setNormalUnretweetGreyIcon() {
+    let greyTapImage = UIImage(named: "retweet-icon")
+    retweetButton.setImage(greyTapImage, for: UIControlState.normal)
+  }
+  //--------------------------------------------------------------------------------//
+  
+  
+  //--------------------------------------------------------------------------------//
+  // Helper functions for sending network requests to Twitter POST endpoints
+  func postRequestToFavoritesCreateEndpoint() {
+    // Send a POST request to the POST favorites/create endpoint
+    APIManager.shared.favorite(tweet) { (tweet, error) in
+      if let  error = error {
+        print("Error favoriting tweet: \(error.localizedDescription)")
+      } else if let tweet = tweet {
+        print("Successfully favorited the following Tweet: \n\(tweet.text)")
+      }
+    }
+  }
+  
+  func postRequestToFavoritesDestroyEndpoint() {
+    // Send a POST request to the POST favorites/destroy endpoint
+    APIManager.shared.unfavorite(tweet) { (tweet, error) in
+      if let  error = error {
+        print("Error unfavoriting tweet: \(error.localizedDescription)")
+      } else if let tweet = tweet {
+        print("Successfully unfavorited the following Tweet: \n\(tweet.text)")
+      }
+    }
+  }
+  
+  func postRequestToStatusesRetweetIDEndpoint() {
+    // Send a POST request to the POST statuses/retweet/:id.json endpoint
+    APIManager.shared.retweet(tweet) { (tweet, error) in
+      if let  error = error {
+        print("Error retweeting tweet: \(error.localizedDescription)")
+      } else if let tweet = tweet {
+        print("Successfully retweeting the following Tweet: \n\(tweet.text)")
+      }
+    }
+  }
+  
+  func postRequestToStatusesUnretweetIDEndpoint() {
+    // Send a POST request to the POST statuses/unretweet/:id.json endpoint
+    APIManager.shared.unretweet(tweet) { (tweet, error) in
+      if let  error = error {
+        print("Error unretweeting tweet: \(error.localizedDescription)")
+      } else if let tweet = tweet {
+        print("Successfully unretweeting the following Tweet: \n\(tweet.text)")
+      }
+    }
+  }
+  //--------------------------------------------------------------------------------//
   
   override func awakeFromNib() {
     super.awakeFromNib()
@@ -57,83 +182,7 @@ class TweetCell: UITableViewCell {
   
   override func setSelected(_ selected: Bool, animated: Bool) {
     super.setSelected(selected, animated: animated)
-    
     // Configure the view for the selected state
   }
-  
-  @IBAction func didTapFavorite(_ sender: Any) {
-    // Update the favorite icon and count in local tweet model
-    tweet.favorited = !tweet.favorited!
-    
-    if tweet.favorited! {
-      let redTapImage = UIImage(named: "favor-icon-red")
-      favoriteButton.setImage(redTapImage, for: UIControlState.normal)
-      tweet.favoriteCount += 1
-      
-      // Send a POST request to the POST favorites/create endpoint
-      APIManager.shared.favorite(tweet) { (tweet, error) in
-        if let  error = error {
-          print("Error favoriting tweet: \(error.localizedDescription)")
-        } else if let tweet = tweet {
-          print("Successfully favorited the following Tweet: \n\(tweet.text)")
-        }
-      }
-    } else {
-      let greyTapImage = UIImage(named: "favor-icon")
-      favoriteButton.setImage(greyTapImage, for: UIControlState.normal)
-      tweet.favoriteCount -= 1
-      
-      // Send a POST request to the POST favorites/destroy endpoint
-      APIManager.shared.unfavorite(tweet) { (tweet, error) in
-        if let  error = error {
-          print("Error unfavoriting tweet: \(error.localizedDescription)")
-        } else if let tweet = tweet {
-          print("Successfully unfavorited the following Tweet: \n\(tweet.text)")
-        }
-      }
-    }
-    // Update cell UI
-    refreshData()
-  }
-  
-  @IBAction func didTapRetweet(_ sender: Any) {
-    // Update the retweet icon and count in local tweet model
-    tweet.retweeted = !tweet.retweeted
-    if tweet.retweeted {
-      let greenTapImage = UIImage(named: "retweet-icon-green")
-      retweetButton.setImage(greenTapImage, for: UIControlState.normal)
-      tweet.retweetCount += 1
-      
-      // Send a POST request to the POST statuses/retweet/:id.json endpoint
-      APIManager.shared.retweet(tweet) { (tweet, error) in
-        if let  error = error {
-          print("Error retweeting tweet: \(error.localizedDescription)")
-        } else if let tweet = tweet {
-          print("Successfully retweeting the following Tweet: \n\(tweet.text)")
-        }
-      }
-    } else {
-      let greyTapImage = UIImage(named: "retweet-icon")
-      retweetButton.setImage(greyTapImage, for: UIControlState.normal)
-      tweet.retweetCount -= 1
-      
-      // Send a POST request to the POST statuses/unretweet/:id.json endpoint
-      APIManager.shared.unretweet(tweet) { (tweet, error) in
-        if let  error = error {
-          print("Error unretweeting tweet: \(error.localizedDescription)")
-        } else if let tweet = tweet {
-          print("Successfully unretweeting the following Tweet: \n\(tweet.text)")
-        }
-      }
-    }
-    // Update cell UI
-    refreshData()
-    
-  }
-  func refreshData() {
-    favoriteCountLabel.text = String(describing: tweet.favoriteCount)
-    retweetCountLabel.text = String(describing: tweet.retweetCount)
-  }
-  
   
 }
