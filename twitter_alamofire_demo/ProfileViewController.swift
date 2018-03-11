7b9cbba6c5893cefbedf436d0d01b5d952ea0dda
Mavey Ma <maveyma@gmail.com>
2018-03-11 13:49:30 -0700

    AutoLayoutConstrained the Backdrop and Prof Pic

---
+++ twitter_alamofire_demo/ProfileViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ProfileViewController.swift
+//  twitter_alamofire_demo
+//
+//  Created by Mavey Ma on 3/11/18.
+//  Copyright © 2018 Charles Hieger. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    // Do any additional setup after loading the view.
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  
+}
