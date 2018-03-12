ee08cebda8f1d99e3da733b72e8049c1daa6afcc
Mavey Ma <maveyma@gmail.com>
2018-03-12 01:10:33 -0700

    Error for compose button item

---
+++ twitter_alamofire_demo/ComposeViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ComposeViewController.swift
+//  twitter_alamofire_demo
+//
+//  Created by Mavey Ma on 3/12/18.
+//  Copyright © 2018 Charles Hieger. All rights reserved.
+//
+
+import UIKit
+
+class ComposeViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    // Do any additional setup after loading the view.
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+}
